941093e699f762071b4285f4f0a0eef6782991a2
Alawode Samuel <tolulope.alawode@gmail.com>
2023-10-10 14:45:28 +0100

    init files

---
+++ src/test/java/com/coopera/cooperaApp/CooperaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coopera.cooperaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooperaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
