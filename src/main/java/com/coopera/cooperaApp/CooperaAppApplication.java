c270c990a0b4990075f6d0702cdf6d9a27a9f931
SamuelBriggs <tolulope.alawode@gmail.com>
2024-01-14 18:03:10 +0100

    preparing for another round

--- src/main/java/com/coopera/cooperaApp/CooperaAppApplication.java
+++ src/main/java/com/coopera/cooperaApp/CooperaAppApplication.java
@@ -2,13 +2,22 @@ package com.coopera.cooperaApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoRepositories
 
-public class CooperaAppApplication {
+public class CooperaAppApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(CooperaAppApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CooperaAppApplication.class, args);
